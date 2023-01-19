7dae7a8ad15ee6b821e8d6fe966899505ff27fd7
Trung <trunghuynh1999@gmail.com>
2023-01-19 18:35:32 +0700

    add sign message and send transaction

--- src/autonomyIRL.js
+++ src/autonomyIRL.js
@@ -1,15 +1,35 @@
 "use strict";
-
-module.exports = {
-  getAddress,
-  closeWebview,
+const chain = {
+  tez: "tezos",
+  eth: "eip155",
 };
 
-function getAddress(blockchain) {
+function getAddress(chain) {
   return window.flutter_inappwebview.callHandler("getAddress", {
-    block_chain: blockchain,
+    chain: chain,
+  });
+}
+function sendTransaction({ transactions, sourceAddress, chain }) {
+  return window.flutter_inappwebview.callHandler("sendTransaction", {
+    chain: chain,
+    sourceAddress: sourceAddress,
+    transactions: transactions,
+  });
+}
+function signMessage({ payload, sourceAddress, chain }) {
+  return window.flutter_inappwebview.callHandler("signMessage", {
+    payload: payload,
+    sourceAddress: sourceAddress,
+    chain: chain,
   });
 }
 function closeWebview() {
   return window.flutter_inappwebview.callHandler("closeWebview");
 }
+module.exports = {
+  getAddress,
+  closeWebview,
+  signMessage,
+  sendTransaction,
+  chain,
+};
